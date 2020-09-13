0f92c9f03201c7c87257a3c971981ebf89a7974d
subhandp <subhan.dinda.putra@gmail.com>
2020-09-13 09:29:19 +0800

    initial

--- 04.sh
+++ 04.sh
@@ -1,9 +1,4 @@
   
 #!/bin/bash
-
-if [[ $1 == 13 || $1 == 12 || $1 == 11 || $1 == 10 || $1 == 8 ]]; then
-    curl -sL https://deb.nodesource.com/setup_$1.x | sudo -E bash -
-    echo "Done Install. Enjoy.."
-else
-    echo "Error, version not found"
-fi
+curl -sL https://deb.nodesource.com/setup_$1.x | sudo -E bash -
+echo "has install"
